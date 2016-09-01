dd7d787f150665f12244e707e617c13694e6c833
Stephanie Chang <stephaniechang@StephanieMacBook-Air.local>
2016-09-01 08:11:40 +0800

    SolarSystem program

---
+++ SolarSystemDirectory.py
@@ -0,0 +1,10 @@
+‘’’This program show the size and distance of the planets in the solar system with reference to the earth
+
+user enters the planet name.
+
+‘’’
+
+planet = input(‘Enter the planet name (Venus, Mars, jupiter, Neptune, Saturn, Mercury) =‘)
+
+if planet != ‘Venus’:
+print(‘Not a planet’)
