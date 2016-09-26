34066e4b015e26700c8bb98447f2ec0afce93e81
Stephanie Chang <stephaniechang@StephanieMacBook-Air.local>
2016-09-26 22:52:18 +0800

    binary to decimal program

---
+++ PyCharmProj/BinaryToDecimal.py
@@ -0,0 +1,17 @@
+'''
+The program converts a binary to a decimal number.
+'''
+import math
+
+dec = 0
+bin = raw_input('Enter only 1s and 0s (10111): ')
+digit = len(bin)
+intBin = int(bin)
+
+while(sum(map(int,str(bin))) > digit):
+    bin = raw_input('Enter only 1s and 0s (10111): ')
+    digit = len(bin)
+
+for c in range(1, digit+1):
+    dec += int(bin[c-1]) * math.pow(2, digit-c)
+print('Decimal number is ' + str(dec))
