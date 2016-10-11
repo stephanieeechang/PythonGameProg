3b5bf5d680917c1d1e4d5b2c7bf48c902f89c000
Stephanie Chang <stephaniechang@StephanieMacBook-Air.local>
2016-10-11 18:26:35 +0800

    print string in rectangular program

---
+++ PyCharmProj/StringList.py
@@ -0,0 +1,28 @@
+'''
+A function that takes a list of strings and prints them, one per line, in a rectangular frame.
+'''
+def printRectangle(stringList):
+    vertical = len(stringList)
+    horizontal = 0
+    for i in range(len(stringList)):
+        length = len(stringList[i])
+        if length > horizontal:
+            horizontal = len(stringList[i])
+    print('*'*(horizontal+2))
+    for v in range(vertical):
+        print('*', end="")
+        print(stringList[v], end="")
+        for space in range(horizontal - len(stringList[v])):
+            print(' ', end="")
+        print('*')
+    print('*'*(horizontal+2))
+
+
+stringList = []
+string = ''
+while str != '0':
+    str = input('Enter a word (0 to end): ')
+    if str != '0':
+        stringList.append(str)
+
+printRectangle(stringList)
