8ef194f15b3c06a9e709ed7859ed0cfcb3d37f97
Stephanie Chang <stephaniechang@StephanieMacBookAir.local>
2017-03-08 21:27:39 +0800

    object programming, employee class

---
+++ PyCharmProj/Engineers.py
@@ -0,0 +1,73 @@
+class Employee:
+    raise_amt = 1.04
+    num_of_emps = 0
+
+    def __init__(self, first, last, pay):
+        self.first = first
+        self.last = last
+        self.pay = pay
+        self.email = first + '.' + last + '@email.com'
+
+        Employee.num_of_emps += 1
+
+    def fullname(self):
+        return '{} {}'.format(self.first, self.last)
+
+    def apply_raise(self):
+        self.pay = int(self.pay*self.raise_amt)
+
+    @classmethod
+    def set_raise_amt(cls, amount):
+        cls.raise_amt = amount
+
+    @classmethod
+    def from_string(cls, emp_str):
+        first, last, pay = emp_str.split('-')
+        return cls(first, last, pay)
+
+    @staticmethod
+    def is_workday(day):
+        if day.weekday() == 5 or day.weekday() == 6:
+            return False
+        return True
+
+class Developer(Employee):
+    raise_amt = 1.10
+
+    def __init__(self, first, last, pay, prog_lang):
+        super().__init__(first, last, pay)
+        self.prog_lang = prog_lang
+
+
+class Manager(Employee):
+    def __init__(self, first, last, pay, employees=None):
+        super().__init__(first, last, pay)
+        if employees is None:
+            self.employees = []
+        else:
+            self.employees = employees
+
+    def add_emp(self, emp):
+        if emp not in self.employees:
+            self.employees.append(emp)
+
+    def remove_emp(self, emp):
+        if emp in self.employees:
+            self.employees.remove(emp)
+
+    def print_emps(self):
+        for emp in self.employees:
+            print('-->', emp.fullname())
+
+
+class Engineer(Employee):
+    num_of_engineer = 0
+    def __init__(self, first, last, pay, department):
+        super().__init__(first, last, pay)
+        self.department = department
+        Engineer.num_of_engineer += 1
+
+eng_1 = Engineer('Stephanie', 'Chang', 50000, 'Department 1')
+print(eng_1.fullname())
+print(eng_1.department)
+
