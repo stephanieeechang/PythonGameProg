67ce79945a54041a1de45d4866319b4bbac09d55
Stephanie Chang <stephaniechang@StephanieMacBookAir.local>
2017-05-05 12:50:16 +0800

    find and replace main program update

--- PyCharmProj/findReplaceMain.py
+++ PyCharmProj/findReplaceMain.py
@@ -1,10 +1,15 @@
 import sys
 from PyQt5.QtWidgets import QApplication, QDialog
+# import converted UI
 from findReplace import Ui_FindAndReplace
 
+# create an QT application
 app = QApplication(sys.argv)
+# create a dialog window
 window = QDialog()
+# create a findAndReplace dialog (object)
 ui = Ui_FindAndReplace()
+# add the findAndReplace dialog to the main window
 ui.setupUi(window)
 
 window.show()
